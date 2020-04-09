954ffb1f7aedb56051ae2b1393664aa578ed7e8a
rizkyrajitha <rajithagunathilake@gmail.com>
2020-04-09 18:31:35 +0530

    minor changes

--- routes/api/api.routes.js
+++ routes/api/api.routes.js
@@ -27,10 +27,8 @@ exports.ownerdashboard = (req, res) => {
         });
     })
     .catch((err) => {
-      if (err.code === 11000) {
-        console.log("duplicate user");
-        res.status(200).json({ msg: "dupuser" });
-      }
+      console.log(err);
+      res.status(500).send("err");
     });
 };
 
@@ -46,9 +44,8 @@ exports.customerdashboard = (req, res) => {
       Order.findOne({ customerId: req.id })
         .then((doc2) => {
           temppayload = {
-            businessname: doc.businessname,
-            username: doc.username,
-            items: doc.items,
+            name: doc.firstName + " " + doc.lastName,
+            district: doc.district,
             orders: doc2,
           };
 
@@ -59,9 +56,40 @@ exports.customerdashboard = (req, res) => {
         });
     })
     .catch((err) => {
-      if (err.code === 11000) {
-        console.log("duplicate user");
-        res.status(200).json({ msg: "dupuser" });
-      }
+      console.log(err);
+      res.status(500).send("err");
+    });
+};
+
+exports.neworder = (req, res) => {
+  console.log("new order");
+  var datain = req.body;
+  console.log(datain);
+
+  User.findOne({ _id: req.id })
+    .then((doc) => {
+      console.log(doc);
+
+var neworder = new Order({
+    
+})
+
+      Order.findOne({ customerId: req.id })
+        .then((doc2) => {
+          temppayload = {
+            name: doc.firstName + " " + doc.lastName,
+            district: doc.district,
+            orders: doc2,
+          };
+
+          res.status(200).json({ msg: "success", data: temppayload });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("err");
     });
 };
